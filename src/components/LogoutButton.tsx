60ef9eb809089417db62c0b20b22f834a2406709
Pierre Masquilier <pierremasquilier@gmail.com>
2023-01-18 18:42:44 -0400

    configure material UI and use Button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
   return (
-    <>{isAuthenticated && <button onClick={() => logout()}>Sign out</button>}</>
+    <>
+      {isAuthenticated && (
+        <Button variant="contained" onClick={() => logout()}>
+          Sign out
+        </Button>
+      )}
+    </>
   );
 };
 
