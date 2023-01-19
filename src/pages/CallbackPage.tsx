f0f3547d9b39f0dfa74c0a2dd56da0d7796f5b8f
Pierre Masquilier <pierremasquilier@gmail.com>
2023-01-19 09:24:45 -0400

    feat: make CallbackPage more generic

--- src/pages/CallbackPage.tsx
+++ src/pages/CallbackPage.tsx
@@ -1,12 +1,20 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
-import ButtonAppBar from "../components/NavBar";
 //The callback-page.js component will only render the navigation bar and an
 //empty content container to help you create a smooth transition between a route
 //with no content, /callback, to a route with content, such as the /profile page.
 export const CallbackPage = () => {
   return (
     <div>
-      <ButtonAppBar />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Catch them all
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>{" "}
     </div>
   );
 };
